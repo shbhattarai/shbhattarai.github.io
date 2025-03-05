38811290643736ffb09eb94f01ae9469bfb81776
shbhattarai <15702375+shbhattarai@users.noreply.github.com>
2025-03-05 03:42:11 +0000

    Deploying to gh-pages from @ shbhattarai/shbhattarai.github.io@21f98c3d9f5d73645c308159f71fa030cbdcdb16 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -490,14 +490,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%73%72%62%68%61%74%74%61%72%61%69%31%33@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-rss',
@@ -511,14 +504,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=srbhattarai13", "_blank");
         },
       },{
       id: 'light-theme',
